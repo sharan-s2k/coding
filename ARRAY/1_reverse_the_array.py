01b0635eb081ce991c8223408d624e246382e402
ssharan <ssharan@juniper.net>
2023-09-18 19:55:36 +0530

    MODIFIED PYTHON 1_reverse_the_array.py

--- ARRAY/1_reverse_the_array.py
+++ ARRAY/1_reverse_the_array.py
@@ -6,10 +6,7 @@ except:
     if len(ip) == 1:
         ip = list(ip[0])
 print(f"Original array -> {ip}")
-rev = ip.copy()
 lenn = len(ip)
 for i in range(int(lenn/2)):
-    temp = rev[i]
-    rev[i] = ip[lenn-i-1]
-    rev[lenn-i-1] = temp
-print(f"Reversed array -> {rev}")
+    ip[i] , ip[lenn-i-1] = ip[lenn-i-1] , ip[i]
+print(f"Reversed array -> {ip}")
